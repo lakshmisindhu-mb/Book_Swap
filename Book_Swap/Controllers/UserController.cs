0359c6557c590803c86383d2bc45e907f49d34c2
kf-mallikarjun-patil <114468196+kf-mallikarjun-patil@users.noreply.github.com>
2023-05-07 16:48:16 +0530

    Code changes fro Update and Delete User

---
+++ Book_Swap/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using Book_Swap_DL;
+using Book_Swap_Models;
+using Book_Swap_Models.Models;
+using Book_Swap_Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Swap_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly BookSwapContext bookSwapContext;
+        private readonly IUserInterface userInterface;
+        private readonly CrudStatus crudStatus;
+
+        public UserController(BookSwapContext bookSwapContext, IUserInterface userInterface)
+        {
+            this.bookSwapContext = bookSwapContext;
+            this.userInterface = userInterface;
+            crudStatus = new CrudStatus();
+        }
+
+     
+
+        [HttpPut]
+        [Route("UpdateUser")]
+        public JsonResult UpdateUser(User userList)
+        {
+            try
+            {
+                userInterface.UpdateUser(userList);
+                crudStatus.Message = "User Updated Successfully";
+                return new JsonResult(crudStatus);
+            }
+            catch(Exception ex)
+            {
+                crudStatus.Message = "User Update Unsuccessfull due to some issue";
+                return new JsonResult(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("DeleteUser")]
+        public JsonResult DeleteUser(int Id)
+        {
+            try
+            {
+                userInterface.DeleteUser(Id);
+                crudStatus.Message = "User Deleted Successfully";
+                return new JsonResult(crudStatus);
+            }
+            catch (Exception ex)
+            {
+                crudStatus.Message = "User Deletion Unsuccessful due to some issue";
+                return new JsonResult(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("SearchUser")]
+        public JsonResult SearchUser(int Id)
+        {
+            try
+            {
+                userInterface.SearchUser(Id);
+                crudStatus.Message = "User Searched Successfully";
+                return new JsonResult(crudStatus);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Book_Swap_Service/Service/UserService.cs
@@ -0,0 +1,68 @@
+﻿using Book_Swap_DL;
+using Book_Swap_Models;
+using Book_Swap_Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Swap_Service.Service
+{
+    public class UserService:IUserInterface
+    {
+        private readonly BookSwapContext bookSwapContext;
+        public UserService(BookSwapContext bookSwapContext)
+        {
+            this.bookSwapContext = bookSwapContext;
+        }
+     
+        public void UpdateUser(User userList)
+        {
+            try
+            {
+                User user = bookSwapContext.Users.Where(y => y.Id == userList.Id).FirstOrDefault()!;
+                user.UpdatedDate = userList.UpdatedDate;
+                user.UserKey = userList.UserKey;
+                bookSwapContext.Entry(user).State = EntityState.Modified;
+                bookSwapContext.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void DeleteUser(int Id)
+        {
+            try
+            {               
+                User user = bookSwapContext.Users.Where(y => y.Id == Id).FirstOrDefault()!;
+                bookSwapContext.Entry(user).State = EntityState.Deleted;
+                bookSwapContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void SearchUser(int Id)
+        {
+            //BookList book = bookSwapContext.BookLists.Where(y => y.Id == bookList.Id).FirstOrDefault()!; 
+            //book.BookName = bookList.BookName;
+            //book.GenreId = bookList.GenreId;    
+            //book.Author = bookList.Author;
+            //book.Publisher = bookList.Publisher;
+            //book.Edition = bookList.Edition;
+            //book.ReleaseDate = bookList.ReleaseDate;
+            //book.ModifiedDate = DateTime.Now;
+            //bookSwapContext.Entry(book).State = EntityState.Modified;
+            //bookSwapContext.SaveChanges();
+        }
+
+
+
+    }
+}
